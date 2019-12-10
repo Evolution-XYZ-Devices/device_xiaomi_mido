0c32e7480d35885bdd680e4160622d776eb6f322
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2019-12-10 15:37:37 +0000

    mido: prop: Enabled 1080p on YouTube
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -36,7 +36,8 @@ vendor.voice.conc.fallbackpath=deep-buffer \
 vendor.voice.path.for.pcm.voip=true \
 vendor.voice.playback.conc.disabled=true \
 vendor.voice.record.conc.disabled=false \
-vendor.voice.voip.conc.disabled=true
+vendor.voice.voip.conc.disabled=true \
+sys.display-size=1920x1080
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
